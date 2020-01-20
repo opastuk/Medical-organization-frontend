63f5c21c666f7116f7b45209c430134639c3854b
Denba <d.babin2173@gmail.com>
2020-01-20 17:49:33 +0300

    разметка футера

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,34 @@
 <template>
-<div class=""></div>
+  <div>
+    <footer class="footer">
+      <ul class="footer__list footer__list--info">
+        <li class="footer__item">8 (495) 555-67-07</li>
+        <li class="footer__item">info@umed.ru</li>
+      </ul>
+      <ul class="footer__list footer__list--menu">
+        <li class="footer__item"><a class="footer__link">Памятка</a></li>
+        <li class="footer__item"><a class="footer__link">Карьера</a></li>
+        <li class="footer__item"><a class="footer__link">Каталоги</a></li>
+        <li class="footer__item"><a class="footer__link">Каталоги</a></li>
+      </ul>
+      <ul class="footer__list footer__list--sub-menu">
+        <li class="footer__item"><a class="footer__link">Корпоративные презентации</a></li>
+        <li class="footer__item"><a class="footer__link">Политика конфиденциальности</a></li>
+        <li class="footer__item"><a class="footer__link">Реквизиты</a></li>
+      </ul>
+      <span class="footer__promo">©ООО "Юмед" 2019г. Все права защищены.</span>
+    </footer>
+  </div>
 </template>
-
 <script>
-export default {
-  name: 'Footer'
-};
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+ export default class HomePage extends Vue {
+
+ }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
