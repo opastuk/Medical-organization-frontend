ceafba198b95d4db69f7dec6921d085bfdbb14ac
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-01-31 04:14:31 +0300

    миксины работают

--- src/components/PageComponent.vue
+++ src/components/PageComponent.vue
@@ -4,23 +4,24 @@
     <slot name="header"></slot>
   </div>
   <div class="page-content">
-    <div class="page-content__main-navigation">
-      <ul class="main-navigation__list">
-        <li class="main-navigation__item"><a class="main-navigation__link" href="index-asset.html">Главная</a></li>
-        <li class="main-navigation__item"><a class="main-navigation__link" href="news-asset.html">Новости</a></li>
-        <li class="main-navigation__item"><a class="main-navigation__link main-navigation__link--dropdown" href="about-asset.html">О нас</a>
-          <ul class="main-navigation__list main-navigation__list--sub main-navigation__list--hidden">
-            <li class="main-navigation__item main-navigation__item--sub"><a class="main-navigation__link main-navigation__link--sub" href="about-asset.html">О компании</a></li>
-            <li class="main-navigation__item main-navigation__item--sub"><a class="main-navigation__link main-navigation__link--sub">Карьера</a></li>
-            <li class="main-navigation__item main-navigation__item--sub"><a class="main-navigation__link main-navigation__link--sub" href="clients-asset.html">Пациентам</a></li>
-            <li class="main-navigation__item main-navigation__item--sub"><a class="main-navigation__link main-navigation__link--sub" href="props-asset.html"> Реквизиты</a></li>
-            <li class="main-navigation__item main-navigation__item--sub"><a class="main-navigation__link main-navigation__link--sub" href="presentations-asset.html">Презентации</a></li>
-          </ul>
-        </li>
-        <li class="main-navigation__item"><a class="main-navigation__link" href="partners-asset.html">Наши партнеры</a></li>
-        <li class="main-navigation__item"><a class="main-navigation__link" href="catalog-asset.html">Каталог</a></li>
-        <li class="main-navigation__item"><a class="main-navigation__link" href="contacts-asset.html">Контакты</a></li>
-      </ul>
+    <div class="page-content__sidebar">
+        <ul class="sidebar__list">
+          <li class="sidebar__item">
+            <a class="sidebar__link">О компании</a>
+          </li>
+          <li class="sidebar__item">
+            <a class="sidebar__link">Карьера</a>
+          </li>
+          <li class="sidebar__item">
+            <a class="sidebar__link">Пациентам</a>
+          </li>
+          <li class="sidebar__item">
+            <a class="sidebar__link">Ревизиты</a>
+          </li>
+          <li class="sidebar__item">
+            <a class="sidebar__link">Презентации</a>
+          </li>
+        </ul>
     </div>
     <div class="page-content__nested-content">
       <slot name="content"></slot>
@@ -30,22 +31,47 @@
 </template>
 
 <script>
-  import {Vue, Component} from 'vue-property-decorator';
+import { Vue, Component } from 'vue-property-decorator';
 
   @Component
-  export default class Page extends Vue {
+export default class Page extends Vue {
 
   }
 </script>
 
 <style scoped lang="scss">
-  .page-container {
-    display: flex;
-    flex-direction: column;
+  .sidebar {
+    width: 100%;
+    margin-bottom: 50px;
+    box-sizing: border-box;
+    &__link {
+      display: block;
+      padding: 15px;
+      text-decoration: none;
+      color: #0c2230;
+    }
+    &__link:hover,
+    &__link:focus {
+      background-color: #1B98E0;
+      color: #ffffff;
+    }
+    &__list {
+      @include reset-list;
+    }
+    &__item {
+      margin-bottom: 1px;
+      background-color: rgba(0, 141, 204, 0.1);
+    }
+    &__link--current {
+      color: #ffffff;
+    }
+    &__item--current {
+      background-color: #1B98E0;
+    }
   }
-
-  .page-content {
-    display: flex;
-    flex-direction: row;
+  @media (min-width: 1200px) {
+    .sidebar {
+      width: 260px;
+    }
   }
 </style>
