057287083948f1a43e62c2abf578a39b7328eb72
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-02-01 09:04:03 +0300

    разметка политики сделана

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Home from '../views/HomePage.vue';
 import About from '../views/AboutPage.vue';
 import Props from '../views/PropsPage.vue';
+import Terms from '../views/TermsPage.vue';
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/props',
     name: 'props',
     component: Props,
+  },
+  {
+    path: '/terms',
+    name: 'terms',
+    component: Terms,
   },
 ];
 
